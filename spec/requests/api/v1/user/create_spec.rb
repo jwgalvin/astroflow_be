96ae85318936feaf91b5192cb24f1b49d826dfc0
Sully Birashk <sully.birashk@gmail.com>
2022-04-13 16:44:03 -0700

    Create Test for Create a user

---
+++ spec/requests/api/v1/user/create_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Create User" do
+
+  it "create user endpoint" do
+    user_params = { name: 'test',
+                    date_of_birth: Date.today
+                  }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
+
+    user = User.last
+
+    expect(user.name).to eq('test')
+  end
+
+end

4d12d2f5fb59c829803a50772fd87cbd481b2da2
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-15 08:52:50 -0400

    Add: Google Auth route and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  
+  get '/auth/google_oauth2/callback', to: 'users#create'
+
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create] do

6c8521be0e9832db1463af9d136f2a2aab458028
Sam Iavri <sammi80134@gmail.com>
2022-04-15 19:09:57 -0600

    add moon route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-
-  
   get '/auth/google_oauth2/callback', to: 'users#create'
 
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create] do
       end
+      resources :moon
     end
   end
 end

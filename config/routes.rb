d51434d421022224361122f943f74dc507f80cb0
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-15 23:19:23 -0400

    Add: api/v1/horoscope route to index action

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create] do
-      end
+      get "/horoscopes/find", to: 'horoscopes#index'
+      resources :users, only: [:create] 
     end
   end
 end

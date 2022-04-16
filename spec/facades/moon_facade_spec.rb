3ca408ed55fd6ec391dc21cf0ac9f3f6c4bfcd61
Sam Iavri <sammi80134@gmail.com>
2022-04-15 19:10:22 -0600

    moon facade test written

--- spec/facades/moon_facade_spec.rb
+++ spec/facades/moon_facade_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 RSpec.describe MoonFacade do
-  it 'returns an array of moon phases for the month', :vcr do
-    moon_phases = MoonFacade.find_moon_phases('April')
-    expect(moon_phases).to be_an(Array)
-    moon_phases.each do |m|
-      expect(m).to be_a(Moon)
-    end
+  it 'returns a moon phases for a specific date', :vcr do
+    moon = MoonFacade.find_todays_moon
+    expect(moon).to be_a(Moon)
+    expect(moon.date).to eq(Time.now.to_i.to_s)
   end
 end

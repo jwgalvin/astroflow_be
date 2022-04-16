7a5531327dc5293c1ba975993fca39f3f8aa0692
Sully Birashk <sully.birashk@gmail.com>
2022-04-15 18:28:10 -0700

    fix controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -5,13 +5,12 @@ class Api::V1::NotesController < ApplicationController
   end
 
   def create
-    render json: Note.create(date: params[:note][:date], message: params[:note][:message], user_id: params[:user_id])
-    #render json: Note.create(note_params)
+    render json: Note.create(note_params)
   end
 
   private
 
   def note_params
-    params.require(:note).permit(:date, :message)
+    params.require(:note).permit(:date, :message, :user_id)
   end
 end

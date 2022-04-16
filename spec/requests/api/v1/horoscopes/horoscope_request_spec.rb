4a31a7e4298d04c396586ba88cc78f4ccc985e02
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-15 23:39:11 -0400

    Test/Feat: Endpoint returns tomorrows horoscope

--- spec/requests/api/v1/horoscopes/horoscope_request_spec.rb
+++ spec/requests/api/v1/horoscopes/horoscope_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Gets Yesterday, Today, Tomorrow, Horoscopes", :vcr do
     zodiac_name = "Leo"
     day = "today"
     keys = [:date_range, :current_date, :description, :compatibility, :mood, :color, :lucky_number, :lucky_time]
+    todays_date = Date.today.strftime("%B %d, %Y")
 
     get "/api/v1/horoscopes/find?sign=#{zodiac_name}&day=#{day}"
 
@@ -13,5 +14,21 @@ RSpec.describe "Gets Yesterday, Today, Tomorrow, Horoscopes", :vcr do
     expect(response).to be_successful 
     expect(horoscope_result.count).to eq(8)
     expect(horoscope_result.keys).to eq(keys)
+    expect(horoscope_result[:current_date]).to eq(todays_date)
+  end 
+
+  it "gets tommorows horoscope for a zodiac sign" do 
+    zodiac_name = "Leo"
+    day = "tomorrow"
+    keys = [:date_range, :current_date, :description, :compatibility, :mood, :color, :lucky_number, :lucky_time]
+    tomorrows_date = Date.tomorrow.strftime("%B %d, %Y")
+
+    get "/api/v1/horoscopes/find?sign=#{zodiac_name}&day=#{day}"
+
+    horoscope_result = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful 
+    expect(horoscope_result.count).to eq(8)
+    expect(horoscope_result.keys).to eq(keys)
+    expect(horoscope_result[:current_date]).to eq(tomorrows_date)
   end 
 end 

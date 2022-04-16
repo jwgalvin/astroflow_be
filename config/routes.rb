23dfe1eb341b275ecbbd8fc40ee6236415543c1f
Sully Birashk <sully.birashk@gmail.com>
2022-04-15 18:13:22 -0700

    Add the appropriate routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create] do
-        resources :notes, only: [:create]
+        resources :notes, only: [:index, :create]
       end
     end
   end

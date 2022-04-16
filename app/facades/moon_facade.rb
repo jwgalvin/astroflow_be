0e1809a3a9106d5c1e653b98c4bbb6ed989a7060
Sam Iavri <sammi80134@gmail.com>
2022-04-15 19:20:44 -0600

    moon facade for Api consumption

---
+++ app/facades/moon_facade.rb
@@ -0,0 +1,9 @@
+class MoonFacade
+  def self.find_todays_moon
+    today = Time.now.to_i
+    results = MoonService.get_todays_moon(today)
+    results.map do |result|
+      Moon.new(result)
+    end.first
+  end
+end

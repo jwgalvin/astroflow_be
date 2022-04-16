f22c156728ac52ba1168f896c297dd76f631b7a3
Sully Birashk <sully.birashk@gmail.com>
2022-04-15 18:13:11 -0700

    Create Test for Finding a note by date

--- spec/requests/api/v1/note_request_spec.rb
+++ spec/requests/api/v1/note_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Create Note" do
 
-  it "create user endpoint" do
+  it "create note endpoint" do
 
     user = User.create!(name: 'Dani', email: 'simpboi@gmail.com', date_of_birth: Date.today)
 
@@ -14,7 +14,25 @@ RSpec.describe "Create Note" do
 
     post "/api/v1/users/#{user.id}/notes", headers: headers, params: JSON.generate(note: note_params)
 
-    expect(user.notes.first.message).to eq('I am feeling extra sick today, I am getting cramps left and right.')
+    expect(response).to be_successful
+
+    note = JSON.parse(response.body, symbolize_names: true)
+
+    expect(note[:message]).to eq('I am feeling extra sick today, I am getting cramps left and right.')
   end
 
+  it "can call a note by date" do
+
+    user = User.create!(name: 'Dani', email: 'simpboi@gmail.com', date_of_birth: Date.today)
+
+    note = user.notes.create!(date: "2022-04-12", message: 'I am feeling great today')
+
+    get "/api/v1/users/#{user.id}/notes?date=2022-04-12"
+
+    expect(response).to be_successful
+
+    note = JSON.parse(response.body, symbolize_names: true)
+
+    expect(note[:message]).to eq('I am feeling great today')
+  end
 end

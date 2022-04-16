a7cfbf3da742ed7fbb41bcc57b8f6829505f3de7
Sully Birashk <sully.birashk@gmail.com>
2022-04-15 18:13:48 -0700

    Update Controller methods

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::NotesController < ApplicationController
 
+  def index
+    render json: Note.find_by(date: params[:date])
+  end
+
   def create
     render json: Note.create(date: params[:note][:date], message: params[:note][:message], user_id: params[:user_id])
     #render json: Note.create(note_params)
@@ -10,5 +14,4 @@ class Api::V1::NotesController < ApplicationController
   def note_params
     params.require(:note).permit(:date, :message)
   end
-
 end

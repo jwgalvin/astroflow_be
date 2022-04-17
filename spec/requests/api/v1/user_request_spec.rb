9f4e9bcee15c0576fa7ffaec7ea14be3a10fc928
Sam Iavri <sammi80134@gmail.com>
2022-04-17 09:36:00 -0600

    Gem update format in sytax

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Create User" do
-
-  it "create user endpoint" do
+RSpec.describe 'Create User' do
+  it 'create user endpoint' do
     user_params = { name: 'test',
                     email: 'dopeboi69420@aol.com',
-                    date_of_birth: Date.today
-                  }
+                    date_of_birth: Date.today }
 
-    headers = {"CONTENT_TYPE" => "application/json"}
+    headers = { 'CONTENT_TYPE' => 'application/json' }
 
     post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
 

0f474656507072448efb908948a852ec772812a4
Sam Iavri <sammi80134@gmail.com>
2022-04-17 12:45:48 -0600

    index method for controller

--- app/controllers/api/v1/daily_flow_chart_controller.rb
+++ app/controllers/api/v1/daily_flow_chart_controller.rb
@@ -10,6 +10,12 @@ class Api::V1::DailyFlowChartController < ApplicationController
     render json: chart.update(flow_chart_params)
   end
 
+  def index
+    user = User.find_by(email: params[:email])
+    chart = user.daily_flow_charts.find_by(date: params[:date])
+    render json: chart
+  end
+
   def flow_chart_params
     params.require(:daily_flow_chart).permit(:date, :bloating, :cramps, :emotions, :flow_status)
   end

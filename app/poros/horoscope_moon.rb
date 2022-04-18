ff5a7aedb7000253f08787b34b5f822a09d8b345
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-18 10:15:56 -0400

    Add: Horoscope moon poro

---
+++ app/poros/horoscope_moon.rb
@@ -0,0 +1,23 @@
+class HoroscopeMoon 
+  attr_reader :current_date, 
+              :description, 
+              :compatibility, 
+              :mood, 
+              :color, 
+              :lucky_number, 
+              :lucky_time, 
+              :moon,
+              :moon_phase  
+  
+  def initialize(data)
+    @current_date = data[:current_date]
+    @description =  data[:description]
+    @compatibility = data[:compatibility]
+    @mood = data[:mood]
+    @color = data[:color]
+    @lucky_number = data[:lucky_number]
+    @lucky_time = data[:lucky_time]
+    @moon = data[:moon]
+    @moon_phase = data[:moon_phase]
+  end
+end 

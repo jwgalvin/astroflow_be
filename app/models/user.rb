d305137d2210e8bdb668fe85f42c007d3430c01d
Sam Iavri <sammi80134@gmail.com>
2022-04-18 08:50:04 -0600

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
   has_many :notes
   has_many :daily_flow_charts
-  
-  validates_presence_of :name
-  validates_presence_of :email
-  validates_presence_of :date_of_birth
 
+  validates_presence_of :email
 end

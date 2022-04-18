a1f7bdd7c7f6c578d71580f4362035ab60b4633b
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-18 10:15:38 -0400

    Add: get_today_horoscope_moon to controller & facade

--- app/controllers/api/v1/horoscopes_controller.rb
+++ app/controllers/api/v1/horoscopes_controller.rb
@@ -1,12 +1,8 @@
 class Api::V1::HoroscopesController < ApplicationController
   def index 
     zodiac_name = params[:sign]
-    if params[:day] == "today"
-      render json: HoroscopeFacade.get_today_horoscope(zodiac_name)
-    elsif params[:day] == "tomorrow"
-      render json: HoroscopeFacade.get_tomorrow_horoscope(zodiac_name)
-    elsif params[:day] == "yesterday"
-      render json: HoroscopeFacade.get_yesterday_horoscope(zodiac_name)
-    end 
+    render json: { today_horoscope: HoroscopeFacade.get_today_horoscope_moon(zodiac_name), 
+                   yesterday_horoscope: HoroscopeFacade.get_yesterday_horoscope_moon(zodiac_name)
+    }
   end 
 end 
--- app/facades/horoscope_facade.rb
+++ app/facades/horoscope_facade.rb
@@ -1,15 +1,35 @@
 class HoroscopeFacade 
   class << self 
-    def get_today_horoscope(zodiac_sign)
-      HoroscopeService.call_today_horoscope(zodiac_sign)
+    def get_today_horoscope_moon(zodiac_sign)
+      today = Time.now.to_i
+      horoscope_data = HoroscopeService.call_today_horoscope(zodiac_sign)
+      moon_data = results = MoonService.get_moon(today)
+      horoscope(horoscope_data, moon_data)
+    end 
+    
+    def get_yesterday_horoscope_moon(zodiac_sign)
+      yesterday = Time.now.to_i - (60*60*24)
+      horoscope_data = HoroscopeService.call_yesterday_horoscope(zodiac_sign)
+      moon_data = results = MoonService.get_moon(yesterday)
+      horoscope(horoscope_data, moon_data)
     end 
 
     def get_tomorrow_horoscope(zodiac_sign)
       HoroscopeService.call_tomorrow_horoscope(zodiac_sign)
     end 
 
-    def get_yesterday_horoscope(zodiac_sign)
-      HoroscopeService.call_yesterday_horoscope(zodiac_sign)
+    private 
+
+    def horoscope(horoscope_data, moon_data)
+      a = HoroscopeMoon.new(current_date: horoscope_data[:current_date],
+                        description: horoscope_data[:description],
+                        compatibility: horoscope_data[:compatibility],
+                        mood: horoscope_data[:mood],
+                        color: horoscope_data[:color],
+                        lucky_number: horoscope_data[:lucky_number],
+                        lucky_time: horoscope_data[:lucky_time],
+                        moon: moon_data.first[:Moon].first,
+                        moon_phase: moon_data.first[:Phase])
     end 
   end 
 end 

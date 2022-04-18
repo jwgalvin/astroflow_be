761fd31533ff79297cc1d3f4dcfe6f0a61a34202
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-18 10:14:06 -0400

    Test: Endpoint returns Today & Yesterday horoscope w/moon

--- spec/requests/api/v1/horoscopes/horoscope_request_spec.rb
+++ spec/requests/api/v1/horoscopes/horoscope_request_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Gets Yesterday, Today, Tomorrow, Horoscopes", :vcr do
-  it "gets today horoscope for a zodiac sign" do
+  xit "gets today horoscope for a zodiac sign" do
     zodiac_name = "Leo"
-    day = "today"
     keys = [:date_range, :current_date, :description, :compatibility, :mood, :color, :lucky_number, :lucky_time]
     todays_date = Date.today.strftime("%B %d, %Y")
 
-    get "/api/v1/horoscopes/find?sign=#{zodiac_name}&day=#{day}"
+    get "/api/v1/horoscopes/find?sign=#{zodiac_name}"
 
     horoscope_result = JSON.parse(response.body, symbolize_names: true)
 
@@ -17,7 +16,7 @@ RSpec.describe "Gets Yesterday, Today, Tomorrow, Horoscopes", :vcr do
     expect(horoscope_result[:current_date]).to eq(todays_date)
   end
 
-  it "gets tommorows horoscope for a zodiac sign" do
+  xit "gets tommorows horoscope for a zodiac sign" do
     zodiac_name = "Leo"
     day = "tomorrow"
     keys = [:date_range, :current_date, :description, :compatibility, :mood, :color, :lucky_number, :lucky_time]
@@ -33,7 +32,7 @@ RSpec.describe "Gets Yesterday, Today, Tomorrow, Horoscopes", :vcr do
     expect(horoscope_result[:current_date]).to eq(tomorrows_date)
   end
 
-  it "gets yesterdays horoscope for a zodiac sign" do
+  xit "gets yesterdays horoscope for a zodiac sign" do
     zodiac_name = "Leo"
     day = "yesterday"
     keys = [:date_range, :current_date, :description, :compatibility, :mood, :color, :lucky_number, :lucky_time]
@@ -48,4 +47,19 @@ RSpec.describe "Gets Yesterday, Today, Tomorrow, Horoscopes", :vcr do
     expect(horoscope_result.keys).to eq(keys)
     expect(horoscope_result[:current_date]).to eq(yesterdays_date)
   end
+
+  it "gets todays and yesterdays horoscope and moon" do  
+    zodiac_name = "Leo"
+    keys = [:current_date, :description, :compatibility, :mood, :color, :lucky_number, :lucky_time, :moon, :moon_phase]
+    
+
+    get "/api/v1/horoscopes/find?sign=#{zodiac_name}"
+    
+    horoscope_result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(horoscope_result.count).to eq(2)
+    expect(horoscope_result[:today_horoscope].keys.count).to eq(keys.count)
+    expect(horoscope_result[:today_horoscope].keys).to eq(keys)
+  end 
 end

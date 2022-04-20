d86f878c27b59b3176a79d868526d31f916b1043
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-20 15:11:08 -0400

    Change: Flow chart enum status

--- app/models/daily_flow_chart.rb
+++ app/models/daily_flow_chart.rb
@@ -5,5 +5,5 @@ class DailyFlowChart < ApplicationRecord
   validates_presence_of :emotions
   validates_presence_of :flow_status
 
-  enum flow_status: { "light" => 0, "moderate" => 1, "heavy" => 2  }
+  enum flow_status: { "Light" => 0, "Moderate" => 1, "Heavy" => 2  }
 end

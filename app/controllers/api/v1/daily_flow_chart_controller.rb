6846a3bdb8c88badcb3e5d843f08ffd3c0f0528e
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-21 15:40:54 -0400

    Fix: Update create & update action to accept flow_chart

--- app/controllers/api/v1/daily_flow_chart_controller.rb
+++ app/controllers/api/v1/daily_flow_chart_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::DailyFlowChartController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    user.daily_flow_charts.create(flow_chart_params)
+    data = parse_data(params["daily_flow_chart"])
+    create_flowchart(user, data)
   end
 
   def update
+    
     user = User.find_by(email: params[:email])
     chart = user.daily_flow_charts.find_by(date: params[:date])
-    chart.update(flow_chart_params)
-    render json: DailyFlowChartSerializer.new(chart)
+    data = parse_data(params["daily_flow_chart"])
+    update_flowchart(chart, data)
   end
 
   def index
@@ -19,9 +21,22 @@ class Api::V1::DailyFlowChartController < ApplicationController
 
   private 
 
-  def flow_chart_params
-    params.permit(:date, :bloating, :cramps, :emotions, :flow_status)
-  end
+  
 
+  def parse_data(data)
+    JSON.parse(data, symbolize_names: true)
+  end 
+
+  def create_flowchart(user, flow_chart) 
+    user.daily_flow_charts.create(date: flow_chart[:date], bloating: flow_chart[:bloating], 
+                                  cramps: flow_chart[:cramps], emotions: flow_chart[:emotions], 
+                                  flow_status: flow_chart[:flow_status])
+  end 
+
+  def update_flowchart(chart, flow_chart) 
+    chart.update(date: flow_chart[:date], bloating: flow_chart[:bloating], 
+                cramps: flow_chart[:cramps], emotions: flow_chart[:emotions], 
+                flow_status: flow_chart[:flow_status])
+  end 
 end
 #http://localhost:5000/api/v1/users/daily_flow_chart?email=#{@user_email}  

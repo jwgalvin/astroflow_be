847d369ff9dece2639c2feaf2a7393bfe69691b6
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-11 17:52:26 -0600

    Feat:added serializer to horoscope controller.

--- app/controllers/api/v1/horoscopes_controller.rb
+++ app/controllers/api/v1/horoscopes_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::HoroscopesController < ApplicationController
-  def index 
+  def index
     zodiac_name = params[:sign]
-    render json: { today_horoscope: HoroscopeFacade.get_today_horoscope_moon(zodiac_name), 
-                   yesterday_horoscope: HoroscopeFacade.get_yesterday_horoscope_moon(zodiac_name)
-    }
-  end 
-end 
+    today = HoroscopeFacade.get_today_horoscope_moon(zodiac_name)
+    yesterday = HoroscopeFacade.get_yesterday_horoscope_moon(zodiac_name)
+    render json: HoroscopeSerializer.new(today, yesterday)
+
+    # { today_horoscope: HoroscopeFacade.get_today_horoscope_moon(zodiac_name),
+    #                yesterday_horoscope: HoroscopeFacade.get_yesterday_horoscope_moon(zodiac_name)
+    # }
+  end
+end

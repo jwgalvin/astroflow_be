0100f17722073ca9e12a27c5fd71fd48822299f2
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-12 21:05:58 -0600

    fix flow_chart controller

--- app/controllers/api/v1/daily_flow_chart_controller.rb
+++ app/controllers/api/v1/daily_flow_chart_controller.rb
@@ -2,15 +2,14 @@ class Api::V1::DailyFlowChartController < ApplicationController
   def create
     #binding.pry
     user = User.find_or_create_by(email: params[:email])
-    data = parse_data(params["daily_flow_chart"])
-    create_flowchart(user, data)
+    #data = parse_data(params["daily_flow_chart"])
+    create_flowchart(user, params["daily_flow_chart"])
   end
 
   def update
     user = User.find_by(email: params[:email])
     chart = user.daily_flow_charts.find_by(date: params[:date])
-    #
-    #
+    #data = parse_data(params["daily_flow_chart"])
     update_flowchart(chart, params["daily_flow_chart"])
   end
 

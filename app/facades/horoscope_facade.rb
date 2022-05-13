c50c5de86aaece77f9b3b41fd766615b598b0f00
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-12 20:10:31 -0600

    Feat: fix Poro capitalization

--- app/facades/horoscope_facade.rb
+++ app/facades/horoscope_facade.rb
@@ -22,32 +22,7 @@ class HoroscopeFacade
       moon_today  = MoonService.get_moon(today)
       moon_yesterday  = MoonService.get_moon(yesterday)
       the_thing = HoroscopeMoon.new(horoscope_today, horoscope_yesterday, moon_today, moon_yesterday)
-      #binding.pry
     end
 
-    private
-
-      # a = HoroscopeMoon.new({today: {
-      #                   current_date: horoscope_today[:current_date],
-      #                   description: horoscope_today[:description],
-      #                   compatibility: horoscope_today[:compatibility],
-      #                   mood: horoscope_today[:mood],
-      #                   color: horoscope_today[:color],
-      #                   lucky_number: horoscope_today[:lucky_number],
-      #                   lucky_time: horoscope_today[:lucky_time],
-      #                   moon: moon_today.first[:Moon].first,
-      #                   moon_phase: moon_today.first[:Phase]},
-      #                 yesterday: {
-      #                   current_date: horoscope_yesterday[:current_date],
-      #                   description: horoscope_yesterday[:description],
-      #                   compatibility: horoscope_yesterday[:compatibility],
-      #                   mood: horoscope_yesterday[:mood],
-      #                   color: horoscope_yesterday[:color],
-      #                   lucky_number: horoscope_yesterday[:lucky_number],
-      #                   lucky_time: horoscope_yesterday[:lucky_time],
-      #                   moon: moon_yesterday.first[:Moon].first,
-      #                   moon_phase: moon_yesterday.first[:Phase]
-      #                   }})
-      #
   end
 end
--- app/poros/horoscope_moon.rb
+++ app/poros/horoscope_moon.rb
@@ -1,5 +1,5 @@
 class HoroscopeMoon
-  attr_reader :current_date,
+  attr_reader :todays_date,
               :description,
               :compatibility,
               :mood,
@@ -19,23 +19,23 @@ class HoroscopeMoon
               :prior_moon_phase
 
   def initialize(horoscope_today, horoscope_yesterday, moon_today, moon_yesterday)
-    @todays_date = horoscope_today[:current_date],
-    @description =  horoscope_today[:description],
-    @compatibility = horoscope_today[:compatibility],
-    @mood = horoscope_today[:mood],
-    @color = horoscope_today[:color],
-    @lucky_number = horoscope_today[:lucky_number],
-    @lucky_time = horoscope_today[:lucky_time],
-    @moon = moon_today[0][:moon],
-    @moon_phase = moon_today[0][:moon_phase],
-    @prior_date = horoscope_yesterday[:current_date],
-    @prior_description =  horoscope_yesterday[:description],
-    @prior_compatibility = horoscope_yesterday[:compatibility],
-    @prior_mood = horoscope_yesterday[:mood],
-    @prior_color = horoscope_yesterday[:color],
-    @prior_lucky_number = horoscope_yesterday[:lucky_number],
-    @prior_lucky_time = horoscope_yesterday[:lucky_time],
-    @prior_moon = moon_yesterday[0][:moon],
-    @prior_moon_phase = moon_yesterday[0][:moon_phase]
+    @todays_date = horoscope_today[:current_date]
+    @description =  horoscope_today[:description]
+    @compatibility = horoscope_today[:compatibility]
+    @mood = horoscope_today[:mood]
+    @color = horoscope_today[:color]
+    @lucky_number = horoscope_today[:lucky_number]
+    @lucky_time = horoscope_today[:lucky_time]
+    @moon = moon_today[0][:Moon]
+    @moon_phase = moon_today[0][:Phase]
+    @prior_date = horoscope_yesterday[:current_date]
+    @prior_description =  horoscope_yesterday[:description]
+    @prior_compatibility = horoscope_yesterday[:compatibility]
+    @prior_mood = horoscope_yesterday[:mood]
+    @prior_color = horoscope_yesterday[:color]
+    @prior_lucky_number = horoscope_yesterday[:lucky_number]
+    @prior_lucky_time = horoscope_yesterday[:lucky_time]
+    @prior_moon = moon_yesterday[0][:Moon]
+    @prior_moon_phase = moon_yesterday[0][:Phase]
   end
 end

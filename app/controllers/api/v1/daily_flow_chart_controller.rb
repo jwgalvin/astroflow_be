8f75f8f623833eee19d54d738e93d6e8237b2a5d
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-12 21:07:19 -0600

    Controller: commented out some more code.

--- app/controllers/api/v1/daily_flow_chart_controller.rb
+++ app/controllers/api/v1/daily_flow_chart_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::DailyFlowChartController < ApplicationController
   def create
-    #binding.pry
     user = User.find_or_create_by(email: params[:email])
     #data = parse_data(params["daily_flow_chart"])
     create_flowchart(user, params["daily_flow_chart"])
@@ -14,7 +13,6 @@ class Api::V1::DailyFlowChartController < ApplicationController
   end
 
   def index
-    #binding.pry
     user = User.find_by(email: params[:email])
     render json: user.daily_flow_charts
   end
@@ -29,9 +27,9 @@ class Api::V1::DailyFlowChartController < ApplicationController
 
 
 
-  def parse_data(data)
-    JSON.parse(data, symbolize_names: true)
-  end
+  # def parse_data(data)
+  #   JSON.parse(data, symbolize_names: true)
+  # end
 
   def create_flowchart(user, flow_chart)
     user.daily_flow_charts.create(date: flow_chart[:date], bloating: flow_chart[:bloating],

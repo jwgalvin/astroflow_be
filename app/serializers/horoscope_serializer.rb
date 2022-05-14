41dd3f4e990ffeef95bb97bbad839a3cb90c62e1
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-14 13:38:14 -0600

    remove pry

--- app/serializers/horoscope_serializer.rb
+++ app/serializers/horoscope_serializer.rb
@@ -2,5 +2,4 @@ class HoroscopeSerializer
   include JSONAPI::Serializer
   set_id :current_date
   attributes :current_date, :color, :compatibility, :description, :mood, :color, :lucky_time, :lucky_number, :moon, :moon_phase
-  #binding.pry
 end
